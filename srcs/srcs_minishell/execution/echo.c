0aa938e4aab859916d2866650a700776c551f775
obouhlel Home <oussama_bouhlel@outlook.fr>
2023-02-01 11:13:40 +0100

    creat echo fonction

--- srcs/srcs_minishell/execution/echo.c
+++ srcs/srcs_minishell/execution/echo.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: obouhlel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/01 10:04:12 by obouhlel          #+#    #+#             */
-/*   Updated: 2023/02/01 11:00:24 by obouhlel         ###   ########.fr       */
+/*   Created: 2023/02/01 11:00:40 by obouhlel          #+#    #+#             */
+/*   Updated: 2023/02/01 11:06:21 by obouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-void	ft_main_execution(void)
+void	ft_echo_exec(t_list *cmd, int mode)
 {
-	
+	if (mode == 1)
+	{
+		ft_putstr_fd(cmd->next->next->content, STDOUT_FILENO);
+	}
+	else
+	{
+		ft_putendl_fd(cmd->content, STDOUT_FILENO);
+	}
 }
