40fc92dfb0db5c71449b374f2d75d7bf40967896
obouhlel <oussama_bouhlel@outlook.fr>
2023-02-25 15:25:32 +0100

    getenv for my environnememt

---
+++ srcs/srcs_minishell/execution/env/ft_get_env.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_env.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouhlel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/25 15:21:18 by obouhlel          #+#    #+#             */
+/*   Updated: 2023/02/25 15:24:41 by obouhlel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../../includes/minishell.h"
+
+char	*ft_get_env(char *name, char **env)
+{
+	char	*value;
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen((const char *)name);
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(name, env[i], len) == 0)
+		{
+			value = ft_get_value(env[i]);
+			if (!value)
+				return (NULL);
+		}
+		i++;
+	}
+	return (NULL);
+}
