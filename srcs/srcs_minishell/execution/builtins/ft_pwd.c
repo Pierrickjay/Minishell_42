5629c6d74229354f80509616a7c40ecf8e429419
obouhlel <oussama_bouhlel@outlook.fr>
2023-03-02 12:24:22 +0100

    pwd patching

--- srcs/srcs_minishell/execution/builtins/ft_pwd.c
+++ srcs/srcs_minishell/execution/builtins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: obouhlel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 13:09:44 by obouhlel          #+#    #+#             */
-/*   Updated: 2023/02/27 10:11:27 by obouhlel         ###   ########.fr       */
+/*   Updated: 2023/03/02 12:23:14 by obouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ int	ft_pwd(t_exec *exec)
 {
 	char	*pwd;
 
-	pwd = ft_getenvi("PWD", exec->envi);
+	(void)exec;
+	pwd = getcwd(NULL, 0);
 	if (!pwd)
-		return (EXIT_FAILURE);
+		return (ft_msg(exec, NULL, errno, &exit), EXIT_FAILURE);
 	ft_putendl_fd(pwd, STDOUT);
+	free(pwd);
 	return (EXIT_SUCCESS);
 }
