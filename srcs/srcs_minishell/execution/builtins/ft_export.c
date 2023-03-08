30a9bfb0f7136777b8d883983a0acb4d5805fee9
obouhlel <oussama_bouhlel@outlook.fr>
2023-03-08 19:16:30 +0100

    Update export bug

--- srcs/srcs_minishell/execution/builtins/ft_export.c
+++ srcs/srcs_minishell/execution/builtins/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: obouhlel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 13:09:28 by obouhlel          #+#    #+#             */
-/*   Updated: 2023/03/07 18:31:07 by obouhlel         ###   ########.fr       */
+/*   Updated: 2023/03/08 19:14:40 by obouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,18 +46,16 @@ static int	ft_export_set(t_exec *exec, char *key, char *value, int type)
 //cat the export value
 static int	ft_export_cat(t_exec *exec, char *key, char *value, int type)
 {
-	int		create;
 	char	*tmp;
 	char	*new_value;
 	size_t	len;
 
-	create = false;
 	len = ft_strlen(key);
 	if (key[len - 1] == '+')
 		key[len - 1] = '\0';
-	tmp = ft_getenvi(key, exec->envi);
-	if (tmp)
+	if (ft_getenvi(key, exec->envi) != NULL)
 	{
+		tmp = ft_getenvi(key, exec->envi);
 		new_value = ft_strjoin(tmp, value);
 		if (!value)
 			return (EXIT_FAILURE);
@@ -65,6 +63,8 @@ static int	ft_export_cat(t_exec *exec, char *key, char *value, int type)
 		if (ft_export_set(exec, key, new_value, type) == EXIT_FAILURE)
 			return (EXIT_FAILURE);
 	}
+	else if (ft_export_set(exec, key, value, type) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
 
