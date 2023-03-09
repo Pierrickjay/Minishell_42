6734d073925dc35bd9b6e1a00bad002d33155a1e
Oussama BOUHLEL <obouhlel@e1r1p1.clusters.42paris.fr>
2023-03-09 13:41:08 +0100

    Update bug vars

--- srcs/srcs_minishell/execution/args.c
+++ srcs/srcs_minishell/execution/args.c
@@ -6,7 +6,7 @@
 /*   By: obouhlel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 12:59:23 by obouhlel          #+#    #+#             */
-/*   Updated: 2023/03/09 11:31:40 by obouhlel         ###   ########.fr       */
+/*   Updated: 2023/03/09 13:36:37 by obouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,67 @@ size_t	ft_args_size(t_list *lst)
 	return (size);
 }
 
+static int	ft_check_space(t_list *lst)
+{
+	while (lst && lst->type != CMD)
+		lst = lst->next;
+	if (lst && lst->type == CMD)
+	{
+		if (ft_strcmp(lst->content, "export") == 0)
+			return (0);
+		if (ft_strchr(lst->content, ' '))
+			return (1);
+		lst = lst->next;
+	}
+	while (lst && lst->type == ARG)
+	{
+		if (ft_strchr(lst->content, ' '))
+			return (1);
+		lst = lst->next;
+	}
+	return (0);
+}
+
+static char	*ft_lst_join_split_bis(t_list *lst, char *str)
+{
+	char	*tmp;
+
+	while (lst && lst->type == ARG)
+	{
+		tmp = str;
+		str = ft_strjoin(str, lst->content);
+		if (!str)
+			return (free(tmp), NULL);
+		free(tmp);
+		tmp = str;
+		str = ft_strjoin(str, " ");
+		if (!str)
+			return (free(tmp), NULL);
+		free(tmp);
+		lst = lst->next;
+	}
+	return (str);
+}
+
+static char	**ft_lst_join_split(t_list *lst)
+{
+	char	*str;
+	char	**args;
+
+	if (lst == NULL)
+		return (NULL);
+	str = ft_strjoin(lst->content, " ");
+	if (!str)
+		return (NULL);
+	lst = lst->next;
+	str = ft_lst_join_split_bis(lst, str);
+	if (!str)
+		return (NULL);
+	args = ft_split(str, ' ');
+	free(str);
+	return (args);
+}
+
 // convert the list to an array of arguments
 char	**ft_lst_to_args(t_list *lst)
 {
@@ -44,6 +105,8 @@ char	**ft_lst_to_args(t_list *lst)
 
 	if (lst == NULL)
 		return (NULL);
+	if (ft_check_space(lst))
+		return (ft_lst_join_split(lst));
 	size = ft_args_size(lst);
 	args = (char **)malloc(sizeof(char *) * (size + 1));
 	if (!args)
@@ -53,7 +116,7 @@ char	**ft_lst_to_args(t_list *lst)
 		lst = lst->next;
 	while (lst && i < size)
 	{
-		args[i] = lst->content;
+		args[i] = ft_strdup(lst->content);
 		lst = lst->next;
 		i++;
 	}
