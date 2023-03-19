b38a673ff011e73108c92b29d1cd52f4a36cdf31
obouhlel <oussama_bouhlel@outlook.fr>
2023-03-19 11:00:20 +0100

    Patch cd bad print when cd -

--- srcs/srcs_minishell/execution/builtins/ft_cd_bis.c
+++ srcs/srcs_minishell/execution/builtins/ft_cd_bis.c
@@ -6,7 +6,7 @@
 /*   By: obouhlel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 09:40:03 by obouhlel          #+#    #+#             */
-/*   Updated: 2023/03/19 10:24:55 by obouhlel         ###   ########.fr       */
+/*   Updated: 2023/03/19 10:59:47 by obouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,10 +96,10 @@ int	ft_cd_back(t_shell *shell)
 		return (ft_msg(NULL, "cd", errno, NULL), 1);
 	if (chdir(ft_getenvi("OLDPWD", shell->envi)))
 		return (ft_msg(NULL, "cd", errno, NULL), 1);
-	ft_putendl_fd(oldpwd, STDOUT);
 	pwd = getcwd(NULL, 0);
 	if (!pwd)
 		return (free(oldpwd), ft_msg(NULL, "cd", errno, NULL), 1);
+	ft_putendl_fd(pwd, STDOUT);
 	if (ft_cd_update_oldpwd(shell, oldpwd))
 		return (EXIT_FAILURE);
 	if (ft_cd_update_pwd(shell, pwd))
